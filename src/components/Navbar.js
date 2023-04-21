b2493c705b9fa0b6301da174f3b82ead64f746c5
Akshitha <chinthakindiakshitha@gmail.com>
2023-04-21 19:30:11 +0530

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -85,7 +85,7 @@ function NavBar() {
                 // as={Link}
                 // to="https://drive.google.com/file/d/1iX6aYdHTdr-mFDdk6A8yU5NNwbFlDd3O/view?usp=sharing"
                 // onClick={() => updateExpanded(false)}
-                href="https://drive.google.com/file/d/1Q-YUlw5O74D0pg9aVRETQD2Ko9OmHng0/view?usp=sharing"
+                href="https://drive.google.com/file/d/1EmZSMGs8OP-Y_PFu2uQBPA7zQpXAtn0N/view?usp=sharing"
                 target="_blank"
                 onClick={() => updateExpanded(false)}
               >
